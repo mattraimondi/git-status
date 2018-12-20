bece49981965d034653885260ef85e953b305c1d
Matthew <huggyraimondi@gmail.com>
2018-12-19 20:31:15 -0500

    update 2.2

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # git-status
-If you're like me, you keep your git repositories all in one folder. But when working on multiple repositories, it can become hard to remember where you left off in each one. By navigating to said folder, invoking this command will list all of your repos in the folder, and the status of them. **This program does not check for unpushed commits.**
+If you're like me, you keep your git repositories all in one folder. But when working on multiple repositories, it can become hard to remember where you left off in each one. By navigating to said folder, invoking this command will list all of your repos in the folder, and their status. **This program does not check for unpushed commits.**
 
 ## Installation
 ```
--- git-status.py
+++ git-status.py
@@ -1,26 +1,58 @@
 #!/usr/bin/env python3
-#version 2.1
+
+# Version 2.2
+
+# 2018 Matthew Raimondi
+# www.mattraimondi.com
+# www.github.com/mattraimondi
+
+#MIT License
+#
+#Copyright (c) 2018 Matthew Raimondi
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+
 import sys
 import subprocess
 import os
 import glob
-class bcolors:
+
+class mcolors:
     PINK = '\033[35m'
     GREEN = '\033[92m'
     RED = '\033[91m'
     ENDC = '\033[0m'
+
 dirs = glob.glob('*' + os.path.sep)
 gits = glob.glob('./*/.git')
 numgits = len(gits)
+
 for i in range(0, numgits):
-    print("\n" + bcolors.PINK + gits[i] + bcolors.ENDC)
+    print("\n" + mcolors.PINK + gits[i] + mcolors.ENDC)
     stats = subprocess.Popen("cd " + dirs[i] +"; git status --porcelain; cd ..", shell=True, stdout=subprocess.PIPE)
     status = stats.communicate()[0]
     stat = str(status)
     if "M" in stat or "??" in stat:
         if "M" in stat:
-            print(bcolors.RED + "Modified Files" + bcolors.ENDC)
+            print(mcolors.RED + "Modified Files" + mcolors.ENDC)
         if "??" in stat:
-            print(bcolors.RED + "Untracked Files" + bcolors.ENDC)
+            print(mcolors.RED + "Untracked Files" + mcolors.ENDC)
     else:
-        print(bcolors.GREEN + "Clean" + bcolors.ENDC)
+        print(mcolors.GREEN + "Clean" + mcolors.ENDC)
